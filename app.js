e57c79675fd87ab52cef133c0b7b7aa21c78deff
fire13542 <fire13542@gmail.com>
2020-06-23 13:21:47 +0300

    add API_URL

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 
-API_UPL = "http://localhost:3000/"
+API_UPL = "https://o-class-virtual-classroom-api.herokuapp.com/"
 
 const authRouter = require('./routes/auth.route');
 const adminRouter = require('./routes/admin.route');
